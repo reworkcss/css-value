59dae34fbeb579db9c22a533f77c8d8af3b5dac7
TJ Holowaychuk <tj@vision-media.ca>
2013-05-07 12:25:39 -0700

    add number parsing

--- index.js
+++ index.js
@@ -2,5 +2,42 @@
 module.exports = parse;
 
 function parse(str) {
-
+  return new Parser(str).parse();
 }
+
+function Parser(str) {
+  this.str = str;
+}
+
+Parser.prototype.skip = function(m){
+  this.str = this.str.slice(m[0].length);
+};
+
+Parser.prototype.number = function(){
+  var m = /^(\d+)(\w+)? */.exec(this.str);
+  if (!m) return;
+  this.skip(m);
+  var n = ~~m[1];
+  var u = m[2];
+
+  return {
+    type: 'number',
+    string: n + u,
+    unit: u,
+    value: n
+  }
+};
+
+Parser.prototype.value = function(){
+  return this.number();
+};
+
+Parser.prototype.parse = function(){
+  var vals = [];
+
+  while (this.str.length) {
+    vals.push(this.value());
+  }
+
+  return vals;
+};
--- test/cases/numbers.js
+++ test/cases/numbers.js
@@ -2,8 +2,8 @@
 exports.string = '1px 0 0 5px';
 
 exports.object = [
-  { type: 'number', string: '1px', unit: 'px', number: 1 },
-  { type: 'number', string: '0', unit: '', number: 0 },
-  { type: 'number', string: '0', unit: '', number: 0 },
-  { type: 'number', string: '1px', unit: 'px', number: 5 },
+  { type: 'number', string: '1px', unit: 'px', value: 1 },
+  { type: 'number', string: '0', unit: '', value: 0 },
+  { type: 'number', string: '0', unit: '', value: 0 },
+  { type: 'number', string: '1px', unit: 'px', value: 5 },
 ];
