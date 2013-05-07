3e43400148f82db448f6ac53d0eda8a646a17fab
TJ Holowaychuk <tj@vision-media.ca>
2013-05-07 15:12:46 -0700

    add string tests

---
+++ test/cases/strings.js
@@ -0,0 +1,7 @@
+
+exports.string = '"foo bar" \'baz\'';
+
+exports.object = [
+  { type: 'string', string: '"foo bar"', value: 'foo bar', quote: '"' },
+  { type: 'string', string: "'baz'", value: 'baz', quote: "'" }
+];
