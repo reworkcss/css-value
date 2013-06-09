30faa9a5803dc665d8db2670400346f303013ff4
TJ Holowaychuk <tj@vision-media.ca>
2013-06-08 19:23:04 -0700

    change test labels

--- test/index.js
+++ test/index.js
@@ -9,10 +9,8 @@ var basename = path.basename;
 readdir('test/cases').forEach(function(file){
   var mod = require(path.resolve('test/cases/' + file));
   var title = basename(file, '.js');
-  describe(title, function(){
-    it('should work', function(){
-      var ret = parse(mod.string);
-      ret.should.eql(mod.object);
-    })
-  });
+  it('should support ' + title, function(){
+    var ret = parse(mod.string);
+    ret.should.eql(mod.object);
+  })
 });
